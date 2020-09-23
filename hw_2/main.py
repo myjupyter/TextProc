9a4338de01572176ce22779225bcb54f0cbce0cb
Ткачук Кирилл Евгеньевич <k.tkachuk@npo-echelon.ru>
2020-09-23 20:59:22 +0300

    Added ngrams

--- hw_2/main.py
+++ hw_2/main.py
@@ -7,12 +7,24 @@ FILE = './stih.txt'
 def filter(data):
     return re.sub('[-!$%^&*()_+|~=`{}\[\]:";\'<>?,.\/\\n\\r\\t#]', ' ', data)
 
+def create_ngram(data, n): 
+    words = re.sub(' +', ' ', filter(data)).split(' ')
+    ngrams = []
+
+    lenght = len(words) - 1
+    for i in range(lenght):
+        if n + i > lenght:
+            break
+        ngrams.append(' '.join(words[i:n+i]))
+
+    return ngrams
+
+
 def main():
     with open(FILE) as file:
         data = file.read()
 
-    print(filter(data))
-
+    print(create_ngram(data, 1))
 
 
 if __name__ == '__main__':
