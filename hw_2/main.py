c321e215a23060a5fb9dfc09466fb7022a25a4b0
Kirill Tkachuk <kirilltchkap@gmail.com>
2020-09-27 18:49:45 +0300

    refactored and fixed code

--- hw_2/main.py
+++ hw_2/main.py
@@ -13,22 +13,16 @@ def filter(data):
 def create_words(data):
     return [word for word in filter(data).split(' ') if len(word) != 0]
 
-
 # создает N-граммы из списка слов
-def create_ngram(words, n):
+def create_ngram(words, n = 2):
     ngrams = []
-    lenght = len(words) - 1
+    lenght = len(words)
     for i in range(lenght):
         if n + i > lenght:
             break
         ngrams.append(' '.join(words[i:n+i]))
 
     return ngrams
-
-# создает N-граммы из текста с предваритеьной обработкой
-def create_ngram_from_text(data, n): 
-    words = create_words(data)
-    return create_ngram(words, n)
 
 # создает словарь частот слов/словосочетаний и т.д.
 def create_freq_dict(words):
@@ -40,100 +34,74 @@ def create_freq_dict(words):
             frequency[word] += 1
     return frequency
 
-# расчет априорной вероятности слова в тексте по Линдстоуну
-def prior_p_Lid(words, l = 1):
-    N = len(words)
-    freq = create_freq_dict(words)
-    V = len(freq)
-   
-    prior = {}
-    for word, c in freq.items():
-       prior[word] = (c + l) / (N + l * V)
+class Dictionary():
+    def __init__(self, text, n = 2):
+        self.n = n
+        self.words = create_words(text)
+        
+        self.ngrams_list = [self.words]
+        self.ngrams_freq_dict_list = [create_freq_dict(self.words)]
+        for i in range(1, n + 1):
+            self.ngrams_list.append(create_ngram(self.words, i + 1))
+            self.ngrams_freq_dict_list.append(create_freq_dict(self.ngrams_list[-1]))
 
-    return prior
+    def P_Lid(self, sentence, l = 0):
+        splitted_sentence = create_words(sentence)
+        word_count = len(splitted_sentence)
+        if word_count > self.n + 1:
+            raise AttributeError('Dictionary has only {:d}-grams'.format(self.n))
+        
+        processed_sentence = ' '.join(splitted_sentence)
 
-# расчет условной вероятности слов в тексте по Линдстоуну
-def cond_p_Lid(ngrams_dict, words_dict, l=1):
-    cond = {}
-    Vb = len(ngrams_dict)
-    for word, word_count in words_dict.items():
-        for ngram, ngram_count in ngrams_dict.items(): 
-            if not ngram.startswith(word):
-                continue
-            first, second = ngram.split(' ')
-            
-            cond[(second, first)] = (ngram_count + l) / (word_count + l * Vb)
-    return cond
+        if (freq := self.ngrams_freq_dict_list[word_count - 1].get(processed_sentence)) is not None:
+            return (freq + l) / (len(self.words) + l * len(self.ngrams_freq_dict_list[word_count - 1]))
+        return l / (len(self.words) + l * len(self.ngrams_freq_dict_list[word_count - 1]))
+
+    def P_Lid_Cond(self, word, sentence, l = 0):
+        splitted_sentence = create_words(sentence)
+        splitted_word     = create_words(word)
+        word_count = len(splitted_sentence)
+        if word_count > self.n:
+            raise AttributeError('Dictionary has only {:d}-grams'.format(word_count))
+       
+        N = len(self.words)
+        C_n = self.P_Lid(' '.join(splitted_sentence + splitted_word), 0) * N 
+        C_n_1 = self.P_Lid(' '.join(splitted_sentence), 0) * N
+        
+        return (C_n + l) / (C_n_1 + l * len(self.ngrams_freq_dict_list[word_count - 1]))
+    
 
 class Text():
-    def __init__(self, filename, n = 2, l = 1):
+    def __init__(self, filename, n = 2):
         with open(filename) as file:
-            data = file.read()
-
-        self.words = create_words(data) 
-        self.N = len(self.words)
-        self.Lambda = l
-
+            raw_text = file.read()
+        
         self.n = n
-        self.ngrams = create_ngram(self.words, n)
-        
-        self.words_freq = create_freq_dict(self.words)
-        self.ngrams_freq = create_freq_dict(self.ngrams)
- 
-        self.prior_p = prior_p_Lid(self.words, self.Lambda)
-        self.cond_p = cond_p_Lid(self.ngrams_freq, self.words_freq)
+        self.dictionary = Dictionary(raw_text, n)
 
+    def next_word_by_Lid_smoothing(self, sentence, l = 0): 
+        max_probability = -(1 << 60)
+        max_probability_word = ""
+        processed_sentence = ' '.join(create_words(sentence)[-self.n:])
+        for word in self.dictionary.words:
+            if (p := self.dictionary.P_Lid_Cond(word, processed_sentence, l)) > max_probability:
+                max_probability = p
+                max_probability_word = word
 
-    def P(self, word):
-        if self.prior_p.get(word) is None:
-            return self.Lambda / (self.N + self.Lambda * len(self.words_freq)) 
-        return self.prior_p[word]
-    
-    def P_cond(self, word_pair):
-        if self.cond_p.get(word_pair) is not None:
-            return self.cond_p[word_pair]
-        else:
-            second, first = word_pair
-            count_first = 0
-            
-            if self.words_freq.get(first) is not None:
-               count_first = self.words_freq[first] 
+        return max_probability_word
 
-            return self.Lambda / (len(self.ngrams_freq) * self.Lambda + count_first) 
-    
-    def next_word(self, sentence, n):
-        words = create_words(sentence) 
-        if len(words) < n:
-            raise RuntimeError('ngrams have no enough words')
-        
-        words = words[-n:]
-        
-        max_p = - 1 << 60
-        that_word = ""
-         
-        for word in self.words:
-            res = math.log(self.P(word)) 
-            for ngram_word in words:
-                #print('ng', ngram_word, word, self.P_cond((ngram_word, word)))
-                res += math.log(self.P_cond((ngram_word, word)))
-            if max_p < res:
-                max_p = res
-                that_word = word
-        return that_word
-        
-    def update_n(n):
-        self.ngrams = create_ngram(self.words, n)
-        self.freq = create_freq_dict(self.ngrams)
+             
+    def next_word_by_WB_smoothing(self, sentence):
+        pass
 
 def main():
-    text = Text('stih.txt', 2, 1)
+    text = Text('big.txt', 3)
 
-    sentence = 'джек'
-    for i in range(10):
-        sentence += ' ' + text.next_word(sentence, 1)
+    sentence = 'А еще были'
+    for i in range(40):
+        sentence += ' ' + text.next_word_by_Lid_smoothing(sentence, 1)
 
     print(sentence)
-    
 
 if __name__ == '__main__':
     main()
