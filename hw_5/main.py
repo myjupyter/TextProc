652f1b358f18a22719aa94c3833bf6f78c9c1c98
myjupyter <kirilltchk@gmail.com>
2020-11-08 23:52:48 +0300

    MI added

--- hw_5/main.py
+++ hw_5/main.py
@@ -28,16 +28,32 @@ class Text:
             if 'NOUN' in w[i].tag.grammemes and any(list(map(lambda x: x in w[i-1].tag.grammemes, adjs))): 
                 bigram_list.append((self.__words[i-1].normal_form, self.__words[i].normal_form)) 
         return Counter(bigram_list) 
+    
+    def unigram(self):
+        return Counter([w.normal_form for w in self.__words])
 
     @property
     def words(self):
         return self.__words
 
+
+def return_MIlist(d, bd):
+    N = len(d)
+    return {b : math.log((bcount**3) * N / (d[b[0]] * d[b[1]])) for b, bcount in bd.items()}
+        
+
 def main():
     text = Text('book.txt')
-    l = sorted(text.bigram().items(), key=lambda x: x[1], reverse=True)
-    with open('freq.res', 'w') as file:
-        file.writelines(list(map(lambda x: ' '.join(list(x[0]))+' '+str(x[1])+'\n' , l))[:20])
+   # l = sorted(text.bigram().items(), key=lambda x: x[1], reverse=True)
+   # with open('freq.res', 'w') as file:
+   #     file.writelines(list(map(lambda x: ' '.join(list(x[0]))+' '+str(x[1])+'\n' , l))[:20])
+
+    d = text.unigram()
+    bd = text.bigram()
+    s = sorted(return_MIlist(d, bd).items(), key=lambda x: x[1], reverse=True) 
+
+    with open('MI.res', 'w') as file:
+        file.writelines(list(map(lambda x: ' '.join(list(x[0]))+' '+str(x[1])+'\n' , s))[:20])
 
 if __name__ == '__main__':
     main()
